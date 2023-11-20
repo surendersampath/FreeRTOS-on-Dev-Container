c988bd6ee05e0ac6c46d1448dde55557967e475b
Surender Sampath <44113702+surendersampath@users.noreply.github.com>
2023-11-20 20:14:37 +0000

    Update main.c
    Updating the main.c to task notification example.

--- src/main.c
+++ src/main.c
@@ -2,19 +2,38 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-// Task that prints "Hello, FreeRTOS!" every 3 seconds
-void periodicPrintTask(void *pvParameters) {
+// Task that Sends Notification "Hello, FreeRTOS!" every 3 seconds
+void sendingTask(void *pvParameters) {
+    TaskHandle_t xTaskToNotify = (TaskHandle_t)pvParameters;
     const TickType_t xDelay = pdMS_TO_TICKS(3000); // Delay in milliseconds (3 seconds)
 
     while (1) {
-        printf("Hello, FreeRTOS!\n");
+        xTaskNotifyGive(xTaskToNotify); // Notify the receiving task
+        printf("Hello, FreeRTOS! Sending Task Notification\n");
         vTaskDelay(xDelay); // Delay for 3 seconds
+
+    }
+}
+
+
+// Task that receives the notification
+void receivingTask(void *pvParameters) {
+    for (;;) {
+        // Wait for a notification indefinitely
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        printf("Notification received!\n");
     }
 }
 
 int main() {
+
+    TaskHandle_t xTaskToNotify;
     // Create a task with priority 1 and stack size 100 words
-    xTaskCreate(periodicPrintTask, "PeriodicPrint", 100, NULL, 1, NULL);
+    xTaskCreate(receivingTask, "PeriodicPrint", 100, NULL, 1, &xTaskToNotify);
+        
+    xTaskCreate(sendingTask, "SendingTask", 100, (void *)xTaskToNotify, 1, NULL);
+
 
     // Start the scheduler
     vTaskStartScheduler();
